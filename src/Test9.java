4e0f537f61464d0a2b32163cb9c03159bd33036d
Gülsüm Uslu <ceylaglsm@gmail.com>
2022-11-22 01:49:07 +0300

    tamamdır

--- src/Test9.java
+++ src/Test9.java
@@ -6,5 +6,7 @@ public class Test9 {
     ([7, 2, 3, 10, 5, 9, 1, 4, 8]) ➞ 6
     ([7, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 10
      */
-
+    public static void main(String[] args) {
+        System.out.println("yeni");
+    }
 }
