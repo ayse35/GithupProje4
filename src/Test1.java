6c4d53d48c4d5c6e744efc85d05b3e191c1f1eb5
TheHatipoglu <ciftciyunusemre20@gmail.com>
2022-12-07 23:27:25 +0300

    07.12.22

--- src/Test1.java
+++ src/Test1.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Test1 {
     /*
     Rastgele sayılardan oluşmuş 100 elemanlık tamsayı dizisindeki tüm elemanların ortalamasını hesaplayan method
     create ediniz...
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        int []arr =new int[100];
+        Random rdm = new Random();
+        int toplam=0;
+        for (int i = 0; i < 100; i++) {
+            arr[i]= rdm.nextInt(1000);
+            toplam+=arr[i];
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("arr.length: " + arr.length);
+        System.out.println("Ortalama: "+(toplam/arr.length));
+
+        System.out.println("Lambda ile cozum: "+(Arrays.stream(arr).sum() / 100));
     }
 }
